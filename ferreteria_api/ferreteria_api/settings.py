29e081a9074952883068854d94f69670895b45ae
Cr1sDC <cr.abusleme@duocuc.cl>
2025-06-07 03:09:38 -0400

    docker hecho

--- ferreteria_api/ferreteria_api/settings.py
+++ ferreteria_api/ferreteria_api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v^0uzg!-9eqq*75(gzl9mct__x$&*q-ppbma3jjrl3n=jpu=!w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
